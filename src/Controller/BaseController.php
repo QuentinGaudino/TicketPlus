7e7338ddf6c0fca0224b932ddb6c27f7eed3d59e
QuentinGaudino <79080238+QuentinGaudino@users.noreply.github.com>
2021-07-06 11:42:48 +0200

    Gestion des droits et redirection en fonction des roles

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,19 +1,48 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class BaseController extends AbstractController
 {
-    /**
-     * @Route("/base", name="base")
+        /**
+     * @Route("/check-user", name="check-user")
      */
-    public function index(): Response
+    public function checkUser(): Response
     {
-        return $this->render('base/index.html.twig', [
+        if ($this->isGranted('ROLE_SUPPORT')){
+            return $this->redirectToRoute('accueil-back');
+        }
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('accueil-front');
+        }
+
+        return $this->render('base/erreurDroits.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+
+    /**
+     * @Route("/front-office", name="accueil-front")
+     * @IsGranted("ROLE_USER")
+     */
+    public function accueilFront(): Response
+    {
+        return $this->render('base/accueilFront.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+
+    /**
+     * @Route("/back-office", name="accueil-back")
+     * @IsGranted("ROLE_SUPPORT")
+     */
+    public function accueilBack(): Response
+    {
+        return $this->render('base/accueilBack.html.twig', [
             'controller_name' => 'BaseController',
         ]);
     }
