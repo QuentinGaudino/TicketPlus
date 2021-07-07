d10e7b932bb1a29133336b0a0af251e8aaf95d56
QuentinGaudino <79080238+QuentinGaudino@users.noreply.github.com>
2021-07-07 15:05:57 +0200

    Création de la page + route permettant de dirigiger les user front vers un création d'incident ou de demandes

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,15 +9,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class BaseController extends AbstractController
 {
         /**
-     * @Route("/check-user", name="check-user")
+     * @Route("/check-user", name="check_user")
      */
     public function checkUser(): Response
     {
         if ($this->isGranted('ROLE_SUPPORT')){
-            return $this->redirectToRoute('accueil-back');
+            return $this->redirectToRoute('accueil_back');
         }
         if ($this->isGranted('ROLE_USER')) {
-            return $this->redirectToRoute('accueil-front');
+            return $this->redirectToRoute('accueil_front');
         }
 
         return $this->render('base/erreurDroits.html.twig', [
@@ -26,7 +26,7 @@ class BaseController extends AbstractController
     }
 
     /**
-     * @Route("/front-office", name="accueil-front")
+     * @Route("/front-office", name="accueil_front")
      * @IsGranted("ROLE_USER")
      */
     public function accueilFront(): Response
@@ -37,7 +37,18 @@ class BaseController extends AbstractController
     }
 
     /**
-     * @Route("/back-office", name="accueil-back")
+     * @Route("/front-office/type-ticket", name="type_ticket")
+     * @IsGranted("ROLE_USER")
+     */
+    public function typeTicket(): Response
+    {
+        return $this->render('base/typeTicket.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+
+    /**
+     * @Route("/back-office", name="accueil_back")
      * @IsGranted("ROLE_SUPPORT")
      */
     public function accueilBack(): Response
