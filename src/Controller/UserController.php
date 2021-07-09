a979c1fed2d970aa3dd67afefa1c22dcd73f1668
QuentinGaudino <79080238+QuentinGaudino@users.noreply.github.com>
2021-07-09 15:21:04 +0200

    Ajout des Roles lors de la crééeation/edit + Hashage des mdp lors de l'edit et route sépcial changement de mdp.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,172 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+/**
+ * @Route("/user")
+ */
 class UserController extends AbstractController
 {
+
+    private $security;
+    private $passwordHasher;
+
+    public function __construct(Security $security, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->security = $security;
+        $this->passwordHasher = $passwordHasher;
+    }
+
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="user_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            //Récupération deu mot de passe
+            $plainPassword = $user->getPassword();
+            //Encodage du dit mot de passe
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                $plainPassword
+            ));
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, User $user, $id, UserRepository $userRepository): Response
+    {
+
+        //Récupération du mail de l'utilisateur connecté (UserIdentifier)
+        $userIdentifierConnected = $this->security->getUser()->getUserIdentifier();
+        //Récupération du User connecté grace à son mail
+        $userConnected = $userRepository->findOneBy(['email' => $userIdentifierConnected]);
+        $userId = $userConnected->getId();
+        
+        //Accès à la page edit de l'utilisateur uniquement si c'est bien sois, ou si Admin sinon redirection vers sa page
+        //Attention aux potentielles boucles infinies
+        
+        if (($userId == $id) || ($this->isGranted('ROLE_ADMIN'))) {
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+
+//TODO: Ne fonctionne pas sur l'edit! A corriger
+
+                $plainPassword = $userConnected->getPassword();
+                $userConnected->setPassword($this->passwordHasher->hashPassword(
+                    $userConnected,
+                    $plainPassword
+                ));
+                $entityManager->persist($userConnected);
+                $entityManager->flush();
+                // $this->getDoctrine()->getManager()->flush();
+    
+                return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+            }
+        
+            return $this->renderForm('user/edit.html.twig', [
+                'user' => $user,
+                'form' => $form,
+            ]);
+        }
+
+        return $this->redirectToRoute('user_edit', ['id' => $userId]);
+
+    }
+
+    /**
+     * @Route("/{id}/edit/password", name="user_edit_password", methods={"GET","POST"})
+     */
+    public function editPassword(Request $request, User $user, $id, UserRepository $userRepository): Response
+    {
+        //Récupération du mail de l'utilisateur connecté (UserIdentifier)
+        $userIdentifierConnected = $this->security->getUser()->getUserIdentifier();
+        //Récupération du User connecté grace à son mail
+        $userConnected = $userRepository->findOneBy(['email' => $userIdentifierConnected]);
+        $userId = $userConnected->getId();
+
+        if (($userId == $id) || ($this->isGranted('ROLE_ADMIN'))) {
+        
+            if ((isset($_POST['password']) && isset($_POST['passwordConfirm'])) && ($_POST['password'] === $_POST['passwordConfirm'])) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $user = $entityManager->getRepository(User::class)->find($id);
+                $newPassword = $_POST['password'];
+                $user->setPassword($this->passwordHasher->hashPassword(
+                    $user,
+                    $newPassword
+                ));
+                $entityManager->flush();
+            } elseif (isset($_POST['password']) && isset($_POST['passwordConfirm'])) {
+                $this->addFlash(
+                    'warning',
+                    'Les deux mot de passe ne correspondent pas'
+                );
+            }
+            return $this->render('user/editPassword.html.twig',[
+                
+            ]);
+        }
+        return $this->redirectToRoute('user_edit_password', ['id' => $userId]);
+    }
+    /**
+     * @Route("/{id}", name="user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Support' => 'ROLE_SUPPORT',
+                    'Manager' => 'ROLE_MANAGER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+            ])
+            ->add('address')
+            ->add('avatarLink')
+            ->add('service')
+        ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
